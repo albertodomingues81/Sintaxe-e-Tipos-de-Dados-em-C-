19d6dc3e4815d7e74bbf3bb3dcb34c031a6fb80b
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2023-03-29 15:50:08 +0000

    Explorando a sintaxe

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ExemploFundamentos.Models; 
+
+Pessoa pessoa1 = new Pessoa ();
+
+
+pessoa1.Nome = "Buta";
+pessoa1.Idade = 26;
+pessoa1.Apresentar();  
+
+
+
